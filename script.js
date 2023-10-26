93e3a5d4c0478f2e4c11af2af03e8cc1e1d0ce61
abdullah <abdullahmohamed.en@gmail.com>
2023-10-27 01:17:36 +0200

    setting up the shop page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let nav = document.getElementById('navbar');
 let bar = document.getElementById('bar') ;
 let close = document.getElementById('close')
-
+let product = document.getElementsByClassName("small-img")
+let showMain = document.getElementById("minImg")
+/* let pro = document.querySelectorAll(".pro") */
 
 if(bar){
     bar.addEventListener('click',() => {
@@ -20,6 +22,21 @@ if(bar){
 
 close.onclick = function(){
     nav.classList.remove('active')
+}
+
+let s 
+product[0].onclick= function(){
+    showMain.src = product[0].src
+}
+
+product[1].onclick= function(){
+    showMain.src = product[1].src
+}
+product[2].onclick= function(){
+    showMain.src = product[2].src
+}
+product[3].onclick= function(){
+    showMain.src = product[3].src
 }
 
 
